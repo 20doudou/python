9f2e2911ff7f6d8c488d1eafa76f4eef42fa6dea
20doudou <1801821259@qq.com>
2026-06-08 17:57:21 +0800

    feat: ?? hello.py ??

---
+++ basics/hello.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+\"\"\"第一个 Python 程序 - Hello World\"\"\"
+
+# 简单的问候
+print('=' * 30)
+print('  Hello, Python!  ')
+print('=' * 30)
+
+# 获取用户输入
+name = input('请输入你的名字: ')
+print(f'你好, {name}! 欢迎学习 Python~')
